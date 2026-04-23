25bbc85790aa15582cf1b96f068b272a292f0629
Shubham Kuchewar <161222983+kshubham5698@users.noreply.github.com>
2026-04-23 11:28:31 +0530

    Add optimal single-pass solution for Best Time to Buy and Sell Stock (O(n))

---
+++ 02-Arrays/BestTimeToBuySellStock/Optimal.java
@@ -0,0 +1,27 @@
+public class Optimal {
+    public static int maxProfit(int[] prices) {
+
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+            if (price < minPrice) {
+                minPrice = price;
+            } else {
+                int profit = price - minPrice;
+                if (profit > maxProfit) {
+                    maxProfit = profit;
+                }
+            }
+        }
+        return maxProfit;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7, 1, 5, 3, 6, 4};
+
+        int result = maxProfit(prices);
+
+        System.out.println("Max Profit: " + result);
+    }
+}
